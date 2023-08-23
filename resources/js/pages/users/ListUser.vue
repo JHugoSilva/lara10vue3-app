90a27f4bec696c583be3ac591b0785b9160219d5
hugo silva <hugo@pop-os.localdomain>
2023-08-23 12:02:27 -0300

    Validação de Form

--- resources/js/pages/users/ListUser.vue
+++ resources/js/pages/users/ListUser.vue
@@ -1,14 +1,16 @@
 <script setup>
-import axios from 'axios'
+import axios from 'axios';
 import { onMounted, reactive, ref } from 'vue';
+import { Form, Field } from 'vee-validate';
+import * as yup from 'yup';
 
 const users = ref([]);
 
-const form = reactive({
-    name: '',
-    email: '',
-    password: ''
-})
+// const form = reactive({
+//     name: '',
+//     email: '',
+//     password: ''
+// })
 
 
 const getUser = () => {
@@ -18,16 +20,31 @@ const getUser = () => {
         })
 }
 
-const createUser = () => {
-    axios.post('/api/users', form)
-    .then((response)=>{
-        users.value.unshift(response.data)
-        form.name = '';
-        form.email = '';
-        form.password = '';
-        $('#createUserModal').modal('hide')
-    })
+const schema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required().min(8)
+})
+
+const createUser = (values, { resetForm }) => {
+    axios.post('/api/users', values)
+    .then((response) => {
+            users.value.unshift(response.data)
+            $('#createUserModal').modal('hide')
+            resetForm()
+        })
 }
+
+// const createUser = () => {
+//     axios.post('/api/users', form)
+//         .then((response) => {
+//             users.value.unshift(response.data)
+//             form.name = '';
+//             form.email = '';
+//             form.password = '';
+//             $('#createUserModal').modal('hide')
+//         })
+// }
 
 onMounted(() => {
     getUser()
@@ -92,26 +109,39 @@ onMounted(() => {
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" class="form-control" id="name" placeholder="Enter Name" v-model="form.name">
+                    <Form @submit="createUser" :validation-schema="schema" v-slot="{ errors }">
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <Field type="text" class="form-control" id="name" :class="{'is-invalid':errors.name}" placeholder="Enter Name"
+                                    name="name"/>
+                                    <span class="valid">{{ errors.name }}</span>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <Field type="email" class="form-control" id="email" :class="{'is-invalid':errors.email}" placeholder="Enter Email"
+                                    name="email"/>
+                                    <span class="valid">{{ errors.email }}</span>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <Field type="password" class="form-control" id="password" :class="{'is-invalid':errors.password}" placeholder="******"
+                                    name="password"/>
+                                    <span class="valid">{{ errors.password }}</span>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" placeholder="Enter Email" v-model="form.email">
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            <button type="submit" class="btn btn-primary" @click="createUser">Save</button>
                         </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" class="form-control" id="password" placeholder="******" v-model="form.password">
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                        <button type="button" class="btn btn-primary" @click="createUser">Save</button>
-                    </div>
+                    </Form>
                 </div>
             </div>
         </div>
     </div>
-</template>
+    </template>
+<style scoped>
+    .valid {
+        color: #dc3545;
+    }
+</style>
